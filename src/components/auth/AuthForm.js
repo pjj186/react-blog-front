1089edbfcfa9e1cb6b56457de7220c0853cdaa4b
Jong jae Park <pjj186@naver.com>
2021-09-27 01:34:53 +0900

    UI 준비하기

---
+++ src/components/auth/AuthForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+// 회원가입, 로그인 폼
+
+const AuthFormBlock = styled.div``;
+
+const AuthForm = () => {
+  return <AuthFormBlock>AuthForm</AuthFormBlock>;
+};
+
+export default AuthForm;
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -5,6 +5,7 @@ const SAMPLE_ACTION = "auth/SAMPLE_ACTION";
 
 // 액션 생성 함수
 // createAction을 사용함으로써 객체를 리턴하는 대신, 간단하게 액션 생성 함수 만들기
+// 예 ) sampleAction(3) 를 호출하면 {type: SAMPLE_ACTION, payload : 3} 요런식으로 액션 객체를 리턴
 export const sampleAction = createAction(SAMPLE_ACTION);
 
 // 이 모듈의 state

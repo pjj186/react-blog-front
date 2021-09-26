b78920589c8431a545fbe06057186dc2ceb1231d
Jong jae Park <pjj186@naver.com>
2021-09-26 22:50:21 +0900

    리덕스 적용

---
+++ src/modules/auth.js
@@ -0,0 +1,21 @@
+import { createAction, handleActions } from "redux-actions";
+
+// 액션 타입
+const SAMPLE_ACTION = "auth/SAMPLE_ACTION";
+
+// 액션 생성 함수
+// createAction을 사용함으로써 객체를 리턴하는 대신, 간단하게 액션 생성 함수 만들기
+export const sampleAction = createAction(SAMPLE_ACTION);
+
+// 이 모듈의 state
+const initialState = {};
+
+// 리듀서
+const auth = handleActions(
+  {
+    [SAMPLE_ACTION]: (state, action) => state,
+  },
+  initialState
+);
+
+export default auth;

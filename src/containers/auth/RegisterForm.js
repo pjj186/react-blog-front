bff3d9116644a45d73f81015813493663d100a2d
Jong jae Park <pjj186@naver.com>
2021-09-30 17:47:15 +0900

    회원가입 구현
    서버와 프론트엔드가 redux-saga를 이용하여 api 요청, 응답을 잘 하는것도 확인할 수 있었다.
    redux-saga가 조금 복잡하긴 하지만 신기하다

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { changeField, initializeForm } from "../../modules/auth";
+import { changeField, initializeForm, register } from "../../modules/auth";
 import AuthForm from "../../components/auth/AuthForm";
 // Register Form 컨테이너
 
@@ -9,8 +9,10 @@ import AuthForm from "../../components/auth/AuthForm";
 const RegisterForm = () => {
   const dispatch = useDispatch();
   // auth <= state.auth
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError } = useSelector(({ auth }) => ({
     form: auth.register, // username, password
+    auth: auth.auth,
+    authError: auth.authError,
   }));
   // 인풋 변경 이벤트 핸들러
   const onChange = (e) => {
@@ -27,13 +29,31 @@ const RegisterForm = () => {
   // 폼 등록 이벤트 핸들러
   const onSubmit = (e) => {
     e.preventDefault();
-    // 구현 예정
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      // 에러
+      return;
+    }
+    dispatch(register({ username, password })); // 액션 디스패치
   };
 
   // 컴포넌트가 처음 렌더링 될 때 form을 초기화
   useEffect(() => {
     dispatch(initializeForm("login"));
   }, [dispatch]);
+
+  // 회언가입 성공/실패 처리
+  useEffect(() => {
+    if (authError) {
+      console.log("오류 발생");
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log("회원가입 성공");
+      console.log(auth);
+    }
+  }, [auth, authError]);
 
   return (
     <AuthForm
